5b82b304a4e5ca4ac2a42694f374faab21e44a63
Loic-1 <loic.vasile130@gmail.com>
2024-12-23 14:38:51 +0100

    Update DB after addition of 'name' column to photo table

--- Projet-DWWM-Loic.sql
+++ Projet-DWWM-Loic.sql
@@ -54,16 +54,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   CONSTRAINT `FK_9474526CA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.comment : ~8 rows (environ)
-INSERT INTO `comment` (`id`, `title`, `content`, `creation_date`, `user_id`, `recipe_id`) VALUES
-	(1, 'comment1', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut amet quia veniam, exercitationem dolorum doloribus maiores. A, iusto suscipit! Explicabo, cupiditate sint quos officia nihil deleniti veritatis excepturi nostrum.', '2024-12-13 16:14:26', 6, 5),
-	(2, 'comment2', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut amet quia veniam, exercitationem dolorum doloribus maiores. A, iusto suscipit! Explicabo, cupiditate sint quos officia nihil deleniti veritatis excepturi nostrum.', '2024-12-13 16:17:24', 5, 5),
-	(3, 'comment1', 'zecfezr\'', '2024-12-13 15:59:41', 5, 5),
-	(4, 'zfz', 'zffz', '2024-12-13 16:00:43', 5, 5),
-	(5, 'fzeqfezq', 'fEZF3Ez', '2024-12-13 16:01:08', 5, 5),
-	(6, 'fzeqfezq', 'xftghhfgygibku', '2024-12-16 07:45:56', 5, 5),
-	(7, 'fzeqfezq', 'aaaaaa', '2024-12-16 07:51:14', 5, 5),
-	(8, 'aaa', 'aaa', '2024-12-16 13:08:19', 5, 6);
+-- Listage des données de la table projet-dwwm-loic.comment : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. compilation
 CREATE TABLE IF NOT EXISTS `compilation` (
@@ -76,13 +67,7 @@ CREATE TABLE IF NOT EXISTS `compilation` (
   CONSTRAINT `FK_AD5F8DF2A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.compilation : ~5 rows (environ)
-INSERT INTO `compilation` (`id`, `name`, `creation_date`, `user_id`) VALUES
-	(25, 'Compilation1', '2024-12-17 15:10:04', 5),
-	(26, 'Compilation2', '2024-12-18 07:39:55', 5),
-	(27, 'user', '2024-12-18 07:44:51', 5),
-	(28, 'Loïc', '2024-12-18 07:48:37', 5),
-	(29, 'user', '2024-12-18 09:07:30', 5);
+-- Listage des données de la table projet-dwwm-loic.compilation : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. contain
 CREATE TABLE IF NOT EXISTS `contain` (
@@ -97,12 +82,7 @@ CREATE TABLE IF NOT EXISTS `contain` (
   CONSTRAINT `FK_4BEFF7C8933FE08C` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.contain : ~4 rows (environ)
-INSERT INTO `contain` (`id`, `recipe_id`, `ingredient_id`, `quantity`) VALUES
-	(5, 5, 3, 10),
-	(6, 5, 6, 1),
-	(7, 5, 7, 0.2),
-	(8, 5, 2, 10);
+-- Listage des données de la table projet-dwwm-loic.contain : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. favorite
 CREATE TABLE IF NOT EXISTS `favorite` (
@@ -115,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `favorite` (
   KEY `IDX_68C58ED9A76ED395` (`user_id`),
   CONSTRAINT `FK_68C58ED959D8A214` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`),
   CONSTRAINT `FK_68C58ED9A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table projet-dwwm-loic.favorite : ~0 rows (environ)
 
@@ -176,17 +156,13 @@ CREATE TABLE IF NOT EXISTS `photo` (
   `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `alt` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `recipe_id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_14B7841859D8A214` (`recipe_id`),
   CONSTRAINT `FK_14B7841859D8A214` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.photo : ~4 rows (environ)
-INSERT INTO `photo` (`id`, `url`, `alt`, `recipe_id`) VALUES
-	(5, 'https://th.bing.com/th/id/OIP.J5F_fEX8aB4O9Hw5WwfE9QHaHa?w=175&h=180&c=7&r=0&o=5&pid=1.7', 'Éclair au chocolat', 7),
-	(6, 'https://www.twofatbellies.com/wp-content/uploads/2011/03/SLR-Feb-March-387.jpg', 'Steak frites', 6),
-	(7, 'https://bing.com/th?id=OSK.996621e554ec268329972d6e4714a4ab', 'Salade niçoise', 5),
-	(8, 'https://th.bing.com/th/id/R.d1c2ae27164edad7393c57e84c328bd8?rik=HfcFN28zMnBQWg&pid=ImgRaw&r=0', 'Salade niçoise', 5);
+-- Listage des données de la table projet-dwwm-loic.photo : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. recipe
 CREATE TABLE IF NOT EXISTS `recipe` (
@@ -202,15 +178,12 @@ CREATE TABLE IF NOT EXISTS `recipe` (
   KEY `IDX_DA88B137A76ED395` (`user_id`),
   CONSTRAINT `FK_DA88B13712469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
   CONSTRAINT `FK_DA88B137A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.recipe : ~5 rows (environ)
+-- Listage des données de la table projet-dwwm-loic.recipe : ~2 rows (environ)
 INSERT INTO `recipe` (`id`, `name`, `preparation_time`, `instructions`, `note`, `category_id`, `user_id`) VALUES
-	(5, 'Salade niçoise', 10, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.', 10, 2, 5),
-	(6, 'Steak frites', 10, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.', 10, 3, 5),
-	(7, 'Éclair au chocolat', 10, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit perferendis amet expedita eligendi dignissimos numquam assumenda adipisci quo! Quae, magni eveniet quaerat provident id temporibus inventore reiciendis ipsum eligendi magnam.', 10, 4, 5),
-	(32, 'Par user2', 42, 'test', NULL, 2, 6),
-	(33, 'Par user2', 42, 'test', NULL, 2, 5);
+	(35, 'Salade niçoise', 15, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error temporibus repudiandae exercitationem provident quos, porro quas ducimus sequi inventore, ut, enim animi nemo illo ipsum similique necessitatibus explicabo eaque recusandae.\r\nMollitia, eos sit nemo aperiam aspernatur autem odit, eaque quam quibusdam rerum aliquid blanditiis expedita maiores. Dicta, sunt expedita! Facilis sed magni placeat nobis assumenda vitae animi dolorem quas molestiae!\r\nRerum fugiat vero sunt placeat illum quos hic in, exercitationem distinctio officia dicta. Magnam magni tempore molestias, repudiandae tempora minus quod velit assumenda, id dolore aliquid aspernatur ratione reiciendis unde?\r\nOdit illo, dolore error tempore dicta officiis maxime enim quo corrupti ipsa magni quae obcaecati quasi magnam culpa cumque delectus saepe ut laboriosam aut numquam sed at accusantium ratione? Nesciunt!\r\nDicta, vero eligendi? Beatae, veniam quod voluptate, tenetur assumenda dicta laborum repellat maxime itaque quas cupiditate! Hic beatae neque et cupiditate. Voluptate alias nesciunt quas et sapiente repellat id. Dicta.', NULL, 2, 5),
+	(36, 'Steak frites', 15, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error temporibus repudiandae exercitationem provident quos, porro quas ducimus sequi inventore, ut, enim animi nemo illo ipsum similique necessitatibus explicabo eaque recusandae.\r\nMollitia, eos sit nemo aperiam aspernatur autem odit, eaque quam quibusdam rerum aliquid blanditiis expedita maiores. Dicta, sunt expedita! Facilis sed magni placeat nobis assumenda vitae animi dolorem quas molestiae!\r\nRerum fugiat vero sunt placeat illum quos hic in, exercitationem distinctio officia dicta. Magnam magni tempore molestias, repudiandae tempora minus quod velit assumenda, id dolore aliquid aspernatur ratione reiciendis unde?\r\nOdit illo, dolore error tempore dicta officiis maxime enim quo corrupti ipsa magni quae obcaecati quasi magnam culpa cumque delectus saepe ut laboriosam aut numquam sed at accusantium ratione? Nesciunt!\r\nDicta, vero eligendi? Beatae, veniam quod voluptate, tenetur assumenda dicta laborum repellat maxime itaque quas cupiditate! Hic beatae neque et cupiditate. Voluptate alias nesciunt quas et sapiente repellat id. Dicta.', NULL, 3, 5);
 
 -- Listage de la structure de table projet-dwwm-loic. recipe_compilation
 CREATE TABLE IF NOT EXISTS `recipe_compilation` (
@@ -223,10 +196,7 @@ CREATE TABLE IF NOT EXISTS `recipe_compilation` (
   CONSTRAINT `FK_79ADC396A5F8C840` FOREIGN KEY (`compilation_id`) REFERENCES `compilation` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.recipe_compilation : ~2 rows (environ)
-INSERT INTO `recipe_compilation` (`compilation_id`, `recipe_id`) VALUES
-	(25, 5),
-	(25, 6);
+-- Listage des données de la table projet-dwwm-loic.recipe_compilation : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. reset_password_request
 CREATE TABLE IF NOT EXISTS `reset_password_request` (
@@ -256,9 +226,7 @@ CREATE TABLE IF NOT EXISTS `save` (
   CONSTRAINT `FK_55663ADEA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.save : ~1 rows (environ)
-INSERT INTO `save` (`id`, `user_id`, `compilation_id`, `register_date`) VALUES
-	(32, 5, 25, '2024-12-18 09:08:52');
+-- Listage des données de la table projet-dwwm-loic.save : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. tag
 CREATE TABLE IF NOT EXISTS `tag` (
@@ -267,10 +235,7 @@ CREATE TABLE IF NOT EXISTS `tag` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.tag : ~2 rows (environ)
-INSERT INTO `tag` (`id`, `name`) VALUES
-	(1, 'Santé!'),
-	(2, 'test');
+-- Listage des données de la table projet-dwwm-loic.tag : ~0 rows (environ)
 
 -- Listage de la structure de table projet-dwwm-loic. tag_compilation
 CREATE TABLE IF NOT EXISTS `tag_compilation` (
@@ -303,7 +268,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 -- Listage des données de la table projet-dwwm-loic.user : ~4 rows (environ)
 INSERT INTO `user` (`id`, `email`, `pseudo`, `password`, `registration_date`, `profile_picture`, `alt`, `biography`, `roles`, `is_verified`) VALUES
-	(5, 'user1@test.fr', 'user1', '$2y$13$MFIFx30jEGVNIWSsN2/6KuLgjgryw5AUuAc8/XFLK/lLE/YsuNwC2', '2024-12-13 07:39:35', NULL, NULL, NULL, '[]', 1),
+	(5, 'user1@test.fr', 'user1', '$2y$13$MFIFx30jEGVNIWSsN2/6KuLgjgryw5AUuAc8/XFLK/lLE/YsuNwC2', '2024-12-13 07:39:35', 'C:\\Users\\Loïc\\AppData\\Local\\Temp\\php39A8.tmp', NULL, 'Je suis user1', '[]', 1),
 	(6, 'user2@test.fr', 'user2', '$2y$13$/xJGxlnjBSHp4gBktAGwxeCXKS0W167WkdIZyGOzZvlQuaOGyvT.y', '2024-12-13 15:02:21', NULL, NULL, NULL, '[]', 0),
 	(7, 'user3@test.fr', 'user3', '$2y$13$LFiKPyHT0Smqj3SE2da6uOw.NhE7BmPGRZRGhUMObQE0vNGLE2mnm', '2024-12-16 14:37:19', NULL, NULL, NULL, '[]', 0),
 	(8, 'user4@test.fr', 'user4', '$2y$13$gyUZQwxdKYwVayycZRpkFugRwP9zDrfZCQZwhM4unLXJn9jEAWF2a', '2024-12-17 15:53:03', NULL, NULL, NULL, '[]', 0);
@@ -319,7 +284,7 @@ CREATE TABLE IF NOT EXISTS `user_followees` (
   CONSTRAINT `FK_816BF4D53AD8644E` FOREIGN KEY (`user_source`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.user_followees : ~1 rows (environ)
+-- Listage des données de la table projet-dwwm-loic.user_followees : ~3 rows (environ)
 INSERT INTO `user_followees` (`user_source`, `user_target`) VALUES
 	(5, 6),
 	(5, 7),
@@ -336,7 +301,7 @@ CREATE TABLE IF NOT EXISTS `user_followers` (
   CONSTRAINT `FK_84E870433AD8644E` FOREIGN KEY (`user_source`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet-dwwm-loic.user_followers : ~1 rows (environ)
+-- Listage des données de la table projet-dwwm-loic.user_followers : ~3 rows (environ)
 INSERT INTO `user_followers` (`user_source`, `user_target`) VALUES
 	(6, 5),
 	(7, 5),
